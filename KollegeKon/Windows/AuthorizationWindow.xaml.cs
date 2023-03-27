f9a1ac202e21a44ffe628661a875ede76ae3216d
Katastrofas <117344267+Katastr0fas@users.noreply.github.com>
2023-03-27 18:36:16 +0300

    Добавьте файлы проекта.

---
+++ KollegeKon/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static KollegeKon.ClassHelper.EFClass;
+
+namespace KollegeKon.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            string text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+            for (int i = 0; i < 5; ++i)
+            {
+                text += ALF[rnd.Next(ALF.Length)];
+            }
+            Capcha.Text = text;
+        }
+
+        private void btnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            var authUser = context.Account.ToList()
+                .Where(i => i.Login == tbLogin.Text && i.Password == pbPass.Password)
+                .FirstOrDefault();
+            if (authUser != null)
+            {
+                if (tBCapcha.Text == Capcha.Text)
+                {
+                    MainWindow main = new MainWindow();
+                    main.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Неверная введена капча");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+
+        private void genCapcha_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            string text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+            for (int i = 0; i < 5; ++i)
+            {
+                text += ALF[rnd.Next(ALF.Length)];
+            }
+            Capcha.Text = text;
+
+
+        }
+
+        private void tblReg_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RegistrationWindow reg = new RegistrationWindow();
+            this.Close();
+            reg.Show();
+        }
+    }
+}
---
+++ KollegeKon/Windows/RegistrationWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static KollegeKon.ClassHelper.EFClass;
+
+namespace KollegeKon.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbLogin.Text))
+            {
+                MessageBox.Show("Логин не может быть пустым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(tbFname.Text))
+            {
+                MessageBox.Show("Имя не может быть пустым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(tbLname.Text))
+            {
+                MessageBox.Show("Фамилия не может быть пустым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(pbPass.Password))
+            {
+                MessageBox.Show("Пароль не может быть пустым");
+
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(dpBirthDay.Text))
+            {
+
+                MessageBox.Show("Дата не может быть пустой");
+                return;
+            }
+
+            var authUser = context.Account.ToList()
+                .Where(i => i.Login == tbLogin.Text && i.Password == pbPass.Password)
+                .FirstOrDefault();
+            if (authUser != null)
+            {
+                MessageBox.Show("Логин занят");
+                return;
+            }
+            DB.Account acc = new DB.Account();
+            acc.Login = tbLogin.Text;
+            acc.Password = pbPass.Password;
+            context.SaveChanges();
+            MessageBox.Show("Ok");
+        }
+    }
+}
